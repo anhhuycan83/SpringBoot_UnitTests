0849d26258e160113cf2a859d2adb895e92dc775
vaneet <vaneetkataria54@gmail.com>
2019-04-02 17:54:42 +0530

    added intial unit test

--- src/test/java/com/kataria/springboot/rest/practice/manager/user/UserResourceManagerTest.java
+++ src/test/java/com/kataria/springboot/rest/practice/manager/user/UserResourceManagerTest.java
@@ -2,11 +2,40 @@ package com.kataria.springboot.rest.practice.manager.user;
 
 import org.junit.jupiter.api.Test;
 
+import com.kataria.springboot.rest.practice.manager.user.beans.User;
+
 public class UserResourceManagerTest {
 
 	@Test
-	void test() {
+	public void getAllUsers() {
 
+	}
+
+	@Test
+	public void getUser() {
+
+	}
+
+	@Test
+	public void addUser() {
+
+	}
+
+	@Test
+	public void deleteUser() {
+
+	}
+
+	@Test
+	private void getMaxId() {
+	}
+
+	@Test
+	public void getAllCorresspondingAddress() {
+	}
+
+	@Test
+	public void getAllJuniors() {
 	}
 
 }
